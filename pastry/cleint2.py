cdec81cad7c2e43a5081a5b456c8afcaf7f45167
avani17101 <avani.gupta@research.iiit.ac.in>
2021-04-30 17:33:17 +0530

    add nameserver attempt:pyro

---
+++ pastry/cleint2.py
@@ -0,0 +1,61 @@
+import Pyro4
+import sys
+import codecs
+
+f_u = open('uri.txt')
+uri = f_u.read()
+uri = uri.replace('\n','')
+f_u.close()
+uri = "PYRO:Pyro.NameServer@localhost:9090"
+Server = Pyro4.Proxy(uri)  # get a Pyro proxy to the greeting object
+
+
+
+class Client(object):
+    def __init__(self):
+        self.sites = set()
+
+    def loop(self, siteName):
+        lis = [1,2,3,4]
+        while(True):
+            action = int(input("choose action to perform- \n 1: enter nodes on new site \n 2: add more nodes to an existing site \n 3: Route a node \n 4: get routing table items \n"))
+            if action not in lis:
+                print("enter number btw 1-4 only")
+            if action == 2: # add more nodes to an existing site
+                numNodes = input('enter number of nodes to enter ')
+                nodes = []
+                for i in range(len(numNodes)):
+                    nodeid = codecs.decode(input('enter node id '), 'unicode_escape')
+                    # nodeid = nodeid*16
+                    nodeid = 'x01'
+                    # ip = input('enter ip ')
+                    ip = '0.0.0.0'
+                    # port = int(input('enter port '))
+                    port = 9999
+                    port = 9999
+                    node = [nodeid, ip, port]
+                    nodes.append(node)
+                Server.add_nodes(nodes)
+                print("added nodes:",nodes)
+                print("routing table elements \n")
+                lis = Server.get_routing_table_items()
+                print(lis)
+                
+            if action == 3:  # route node
+                nodeid = codecs.decode(input('enter node id '), 'unicode_escape')
+                nodeid = nodeid*16
+                res = Server.route_node(nodeid)
+                print(res)
+
+            if action == 4:  # get routing table items
+                lis = Server.get_routing_table_items()
+                print(lis)
+
+
+
+
+       
+    
+
+
+
--- pastry/server.py
+++ pastry/server.py
@@ -1,39 +1,80 @@
 import Pyro4
 from routing import Pastry
+import json
 
 @Pyro4.expose
 class Server(object):
-    def __init__(self):
+    def __init__(self, site, nodes):
+        self.site = site
+        self.peers = None
+        # self.pastry = Pastry(bytes(site, 'utf-8'), peers = self.peers)
         self.pastry = None
+        self.nodes = nodes
         
-
+        
     def add_nodes(self, site, nodes):
         peers = {}
         for node in nodes:
             nodeid, ip, port = node
             peers[bytes(nodeid, 'utf-8')] = (ip, port)
         self.pastry =  Pastry(bytes(site, 'utf-8'), peers = peers)
+   
+    def read_json(self):
+         with open('peers.json') as f:
+            self.peers = json.load(f)
 
-    def update_nodes(self, nodes):
-        peers = {}
-        for node in nodes:
-            nodeid, ip, port = node
-            peers[bytes(nodeid, 'utf-8')] = (ip, port)
-        self.pastry.update(peers)
+    # def add_nodes(self, nodes):
+    #     self.peers = {}
+    #     for node in nodes:
+    #         nodeid, ip, port = node
+    #         self.peers[bytes(nodeid, 'utf-8')] = (ip, port)
+    #     print(self.peers)
+    #     with open('peers.json', 'w') as outfile:
+    #         json.dump(self.peers, outfile)
+    #     self.pastry.update(self.peers)
    
     def route_node(self,nodeid):
-        self.pastry.route(bytes(nodeid, 'utf-8'))
+        res = self.pastry.route(bytes(nodeid, 'utf-8'))
+        return res
 
     def get_routing_table_items(self):
         print(list(self.pastry.routing_table.items()))
         return list(self.pastry.routing_table.items())
 
-daemon = Pyro4.Daemon()         # make a Pyro daemon
-uri = daemon.register(Server)   # register the greeting maker as a Pyro object
+# daemon = Pyro4.Daemon()         # make a Pyro daemon
+# uri = daemon.register(Server)   # register the greeting maker as a Pyro object
 
-f = open('uri.txt','w+')
-print(uri,file=f)
-f.close()
+# daemon = Pyro4.Daemon.serveSimple(
+#     {
+#         Server: "server obj"
+#     },
+#     ns = False)
 
-print("Ready. Object uri =", uri)      # print the uri so we can use it in the client later
-daemon.requestLoop()                   # start the event loop of the server to wait for calls
+# f = open('uri.txt','w+')
+# print(uri,file=f)
+# f.close()
+
+# print("Ready. Object uri =PYRO:Pyro.NameServer@localhost:9090")      # print the uri so we can use it in the client later
+# daemon.requestLoop()                   # start the event loop of the server to wait for calls
+
+
+nodeid = 'x01'
+ip = '0.0.0.0'
+port = 8080
+node = [nodeid, ip, port]
+ob1 = Server('google.com', [node])
+
+nodeid = 'x02'
+ip = '0.0.0.1'
+port = 8000
+node = [nodeid, ip, port]
+ob2 = Server('facebook.com', [node])
+# Server.add_nodes([node], 'googe.com')
+print("running")
+with Pyro4.Daemon() as daemon:
+    google_uri = daemon.register(ob1)
+    fb_uri = daemon.register(ob2)
+    with Pyro4.locateNS() as ns:
+        ns.register("DNS.google", google_uri)
+        ns.register("DNS.facebook", fb_uri)
+    daemon.requestLoop()
